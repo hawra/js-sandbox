d5c518de0d1f90b2611a273086f6321bd260f9cb
Randall Hawkins <randy.hawkins.md@gmail.com>
2023-04-22 17:34:54 -0700

    final, with RSH comments

--- 08-shopping-list-project/02-remove-clear-items/shopping-list/script.js
+++ 08-shopping-list-project/02-remove-clear-items/shopping-list/script.js
@@ -3,7 +3,6 @@ const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
 const itemFilter = document.getElementById('filter');
-const items = itemList.querySelectorAll('li'); //RSH Makes a NodeList
 
 function addItem(e) {
   e.preventDefault();
@@ -43,22 +42,61 @@ function createIcon(classes) {
 }
 
 function removeItem(e) {
+  console.log(
+    e.target.parentElement.classList,
+    'RSH Saturday',
+    e.target.parentElement.parentElement
+  );
+
   if (e.target.parentElement.classList.contains('remove-item')) {
-    e.target.parentElement.parentElement.remove();
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+      checkUI();
+    }
   }
 }
 
-function clearItems() {
+function clearItems(e) {
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild);
+    checkUI();
   }
+}
+
+function filterItems(e) {
+  const items = itemList.querySelectorAll('li'); //RSH Makes a NodeList
+  const text = e.target.value.toLowerCase();
+  console.log(text, items, 'RSH 4');
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+    console.log(itemName, 'RSH'); // RSH log the text in the filter
+    if (itemName.indexOf(text) != -1) {
+      console.log(true);
+      item.style.display = 'flex';
+    } else {
+      console.log(false);
+      item.style.display = 'none';
+    }
+  });
 }
 
 function checkUI() {
+  const items = itemList.querySelectorAll('li'); //RSH Makes a NodeList
+  console.log('RSH Testing to see if there are any list items');
   if (items.length === 0) {
+    console.log(
+      'RSH - No items, so delete Clear All button and Filter Items input'
+    );
+    clearBtn.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
+    clearBtn.style.display = 'block';
+    itemFilter.style.display = 'block';
   }
 }
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
+checkUI();
