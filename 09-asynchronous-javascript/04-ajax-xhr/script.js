069d68e08fc67af239b323409f74ab34cc274d03
Randall Hawkins <randy.hawkins.md@gmail.com>
2023-04-23 20:25:41 -0700

    ajax-xhr, launch.json => to use http live server

--- 09-asynchronous-javascript/04-ajax-xhr/script.js
+++ 09-asynchronous-javascript/04-ajax-xhr/script.js
@@ -1,8 +1,8 @@
 const xhr = new XMLHttpRequest(); // RSH XHR  stands for XmlHttpRequest
 
 // Sepcify method and endpoint/URL
-xhr.open('GET', './movies.json');
-// xhr.open('GET', 'https://api.github.com/users/bradtraversy/repos');
+// xhr.open('GET', './movies.json');
+xhr.open('GET', 'https://api.github.com/users/bradtraversy/repos');
 
 // readyState has 5 possible values
 // - 0: request not initialized
@@ -11,8 +11,20 @@ xhr.open('GET', './movies.json');
 // - 3: processing request
 // - 4: request finished and response is ready
 xhr.onreadystatechange = function () {
-  console.log(this.readyState);
-  //console.log(JSON.parse(this.responseText));
+  if (this.readyState === 4 && this.status === 200) {
+    //    console.log(JSON.parse(this.responseText));
+    const data = JSON.parse(this.responseText);
+    // data.forEach((movie) => {    // RSH comment/uncommment toggle the next 3 lines with the ensuing 3
+    //   const li = document.createElement('li');
+    //   li.innerHTML = `<strong>${movie.title}</strong> - ${movie.year}`;
+    data.forEach((repo) => {
+      // RSH by toggling these 3 lines you switch from pulling from movies/json and brad's github repo
+      const li = document.createElement('li');
+      li.innerHTML = `<strong>${repo.name}</strong> - ${repo.description}`;
+
+      document.querySelector('#results').appendChild(li);
+    });
+  }
 };
 //     const data = JSON.parse(this.responseText);
 
