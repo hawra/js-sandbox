84d925511e7451c948b1e52c04318f21fdb13603
Randall Hawkins <randy.hawkins.md@gmail.com>
2023-04-25 13:29:04 -0700

    RSH Edits 08 async await error handling

--- 10-fetch-and-async-await/08-async-await-error-handling/script.js
+++ 10-fetch-and-async-await/08-async-await-error-handling/script.js
@@ -1,13 +1,13 @@
 const getUsers = async () => {
   try {
-    // const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    const response = await fetch('http://httpstat.us/500');
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    // const response = await fetch('http://httpstat.us/500');
 
-    if (!response.ok) {
-      throw new Error('Request Failed');
-    }
+    // if (!response.ok) {
+    //   throw new Error('Request Failed');
+    // }
 
-    const data = await response.text();
+    const data = await response.json();
 
     console.log(data);
   } catch (error) {
@@ -16,17 +16,18 @@ const getUsers = async () => {
 };
 
 const getPosts = async () => {
-  // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const response = await fetch('http://httpstat.us/500');
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  // const response = await fetch('http://httpstat.us/500');
 
   if (!response.ok) {
     throw new Error('Request Failed');
   }
 
-  const data = await response.text();
+  const data = await response.json();
 
   console.log(data);
 };
 
 // getUsers();
-getPosts().catch((error) => console.log(error));
+getPosts();
+// getPosts().catch((error) => console.log(error));
