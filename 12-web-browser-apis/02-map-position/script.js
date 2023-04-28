e6e8731ae2505cc44640ac77d42d3ad96102e381
Randall Hawkins <randy.hawkins.md@gmail.com>
2023-04-27 17:01:07 -0700

    Web APIs

--- 12-web-browser-apis/02-map-position/script.js
+++ 12-web-browser-apis/02-map-position/script.js
@@ -9,10 +9,15 @@ const marker = L.marker([0, 0]).addTo(map);
 
 navigator.geolocation.getCurrentPosition(function (pos) {
   const lat = pos.coords.latitude;
+  console.log(lat, 'RSH 1'); // RSH
   const lng = pos.coords.longitude;
+  console.log(lng, 'RSH 2'); // RSH
 
   marker.setLatLng([lat, lng]).update();
-  map.setView([lat, lng], 13);
+  map.setView([lat, lng], 15); // Here is where we move the map to plum street
+  // Plum St latitude : 32.7302995 ; longitude : -117.2311412
+  // This actually appears to be next door 3414 Jarvis, not 1604 Plim St.
+  // Our computer room: Lat : 32.73012, Long: -117.23091
 
   marker.bindPopup('<strong>Hello World</strong> <br> This is my location');
 });
--- 12-web-browser-apis/03-canvas/script.js
+++ 12-web-browser-apis/03-canvas/script.js
@@ -7,16 +7,16 @@ ctx.fillRect(10, 10, 150, 100);
 
 // Draw circle
 ctx.fillStyle = 'red';
-ctx.arc(300, 300, 100, 0, Math.PI * 2);
+ctx.arc(300, 300, 100, 0, Math.PI * 2); // The angle is in radians
 ctx.fill();
 
 // Draw lines
 ctx.beginPath();
-ctx.strokeStyle = 'orange';
+ctx.strokeStyle = 'orange'; // The color of the line
 ctx.lineWidth = 5;
-ctx.moveTo(10, 10);
-ctx.lineTo(300, 300);
-ctx.stroke();
+ctx.moveTo(10, 10); // go to the origin of the line
+ctx.lineTo(300, 300); // this designates the end of the line
+ctx.stroke(); // This is the code that draws the line
 
 // Draw text
 ctx.font = '30px Arial';
@@ -28,6 +28,6 @@ ctx.strokeText('Hello World', 300, 500, 300);
 
 // Draw image
 const image = document.querySelector('img');
-image.style.display = 'none';
+image.style.display = 'none'; // This hides the image
 
 image.addEventListener('load', () => ctx.drawImage(image, 250, 250, 100, 100));
