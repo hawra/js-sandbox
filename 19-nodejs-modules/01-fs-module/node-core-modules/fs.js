abab40f7ed14c048b5aa1a5ceb8156414e3ee8c0
Randall Hawkins <randy.hawkins.md@gmail.com>
2023-05-06 08:59:28 -0700

    fs (file) module

--- 19-nodejs-modules/01-fs-module/node-core-modules/fs.js
+++ 19-nodejs-modules/01-fs-module/node-core-modules/fs.js
@@ -3,7 +3,7 @@ const fs = require('fs/promises');
 
 // Write to a file //
 
-// Callback version
+// Callback version   // RSH In this version the file gets created and the console log fires, and then if there is an error it is is thrown
 // fs.writeFile('file1.txt', 'Hello World', (err) => {
 //   if (err) throw err;
 //   console.log('File created');
@@ -15,20 +15,20 @@ const fs = require('fs/promises');
 //   .catch((err) => console.log(err));
 
 // Sync version
-// fs.writeFileSync('file3.txt', 'Hello World 3');
-// console.log('File created');
+// fs.writeFileSync('file3.txt', 'Hello World 3 RSH');   // RSH Nothing below this line occurs until the file is created
+// console.log('File created RSH');
 
-// Async/Await
+// Async/Await    // RSH Uses promises
 async function createFile(filename, content) {
   try {
     await fs.writeFile(filename, content);
-    console.log('File created');
+    console.log('File created RSH Using async await, try and catch');
   } catch (error) {
     console.log(error);
   }
 }
 
-// Read From a File //
+// // Read From a File //
 
 async function readFile(filename) {
   try {
@@ -39,17 +39,17 @@ async function readFile(filename) {
   }
 }
 
-// Delete a File //
+// // Delete a File //
 async function deleteFile(filename) {
   try {
     await fs.unlink(filename);
-    console.log(`File ${filename} deleted`);
+    console.log(`File ${filename} deleted on line 46 RSH`);
   } catch (error) {
     console.log(error);
   }
 }
 
-// Rename a File //
+// // Rename a File //
 async function renameFile(oldName, newName) {
   try {
     await fs.rename(oldName, newName);
@@ -59,7 +59,7 @@ async function renameFile(oldName, newName) {
   }
 }
 
-// Create a folder //
+// // Create a folder //
 async function createFolder(folderName) {
   try {
     await fs.mkdir(folderName);
@@ -71,7 +71,7 @@ async function createFolder(folderName) {
 
 createFolder('folder1');
 
-// renameFile('file1.txt', 'file.txt');
+// renameFile('file1.txt', 'fileRSH.txt');
 
 // deleteFile('file4.txt');
 
@@ -79,6 +79,6 @@ createFolder('folder1');
 // readFile('file2.txt');
 // readFile('file3.txt');
 
-// createFile('file4.txt', 'Hello World 4');
+// createFile('file4.txt', 'Hello World 4, RSH Initially from lines 22-30');
 
 // https://nodejs.org/api/fs.html
