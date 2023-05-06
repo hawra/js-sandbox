ec29b6c347f2eafb374bcca8e33edea3056d428a
Randall Hawkins <randy.hawkins.md@gmail.com>
2023-05-06 16:30:35 -0700

    nodemon and Route cleanup

---
+++ 20-randomideas-rest-api/randomideas-api/routes/ideas.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const ideas = [
+  {
+    id: 1,
+    text: 'Positive NewsLetter, a newsletter that only shares positive, uplifting news',
+    tag: 'Technology',
+    username: 'TonyStark',
+    date: '2022-01-02',
+  },
+  {
+    id: 2,
+    text: 'Milk cartons that turn a different color the older that your milk is getting',
+    tag: 'Inventions',
+    username: 'SteveRogers',
+    date: '2022-01-02',
+  },
+
+  {
+    id: 3,
+    text: 'ATM location app which lets you know where the closest ATM is and if it is in service',
+    tag: 'Software',
+    username: 'BruceBanner',
+    date: '2022-01-02',
+  },
+  {
+    id: 4,
+    text: "Blando's - a restaurant where all food is bland and flavorless",
+    tag: 'Societal Needs',
+    username: 'PennyHawk',
+    date: '2023-05-06',
+  },
+];
+
+// Get all ideas
+router.get('/', (req, res) => {
+  res.json({ success: true, data: ideas });
+});
+
+// Get single idea
+router.get('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+  if (!idea) {
+    return res
+      .status(404)
+      .json({ success: false, error: 'Resource not found RSH' });
+  }
+  res.json({ success: true, data: idea });
+});
+
+module.exports = router;
