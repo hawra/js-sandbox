2e56012c629fbaa62461870161c9a2fbdc41ddfa
Randall Hawkins <randy.hawkins.md@gmail.com>
2023-05-08 12:25:26 -0700

    Through Section 20 of Random Ideas Project

--- 20-randomideas-rest-api/randomideas-api/server.js
+++ 20-randomideas-rest-api/randomideas-api/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const port = 5000;
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+
+connectDB();
 
 const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the RandomIdeas API RSH' });
